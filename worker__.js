6d3a5ce079f733ace15f9e8397c62d3b0ab66fb6
Erick Augusto <erick.augusto.ebs@gmail.com>
2021-10-22 12:41:44 -0300

    Mudança na linha 4

--- worker__.js
+++ worker__.js
@@ -1,7 +1,7 @@
  const { parentPort } = require('worker_threads')
  const { spawn } = require('child_process')
  const start = Date.now()
- const pstree = spawn('htop', ['']);
+ const pstree = spawn('htop', []);
 
       
 pstree.stdout.on('data', (data)=>{

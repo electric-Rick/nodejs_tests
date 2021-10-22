800fd4f0c87530fb47adeb96383ef8eb200cde5f
Erick Augusto <erick.augusto.ebs@gmail.com>
2021-10-22 13:23:31 -0300

    1

--- worker__.js
+++ worker__.js
@@ -1,17 +1,19 @@
  const { parentPort } = require('worker_threads')
  const { spawn } = require('child_process')
  const start = Date.now()
- const pstree = spawn('ps', ['-t']);
+ const pstree = spawn('htop', []);
       
 pstree.stdout.on('data', (data)=>{
   console.log(`stdout: ${data}`);
 })
+
+
 pstree.stderr.on('data', (data)=>{
   console.error(`stderr: ${data}`)
 })
 
 pstree.on('close', (code) =>{
-  console.log(`child process exited with code: ${code}`)
+  console.log(`processo ps  exited with code: ${code}`)
 })
 
 
